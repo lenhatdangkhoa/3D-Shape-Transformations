9f745e7c2ea39d0fc02ea5771b0561a1f9bb1cb8
Khoa Le <kl58277@uga.edu>
2022-10-24 23:43:44 -0400

    implement eyeCS conversion

--- draw3D.py
+++ draw3D.py
@@ -3,8 +3,8 @@ import math
 import numpy as np
 import transformations as trans
 # Creating a 250 x 250 plain black image
-image = Image.new(mode="RGB", size = (501, 501), color = (0,0,0))
-
+image = Image.new(mode="RGB", size = (1024, 1024), color = (0,0,0))
+np.set_printoptions(formatter={'float' : lambda x: "{0:0.3f}".format(x)})
 """
 Takes two coordinates and draw a line using the basic line drawing algorithm
 """
@@ -15,7 +15,7 @@ def draw_basic_line(x0, y0, x1, y1):
         smaller_y_value = min(y0,y1)
         # The critical loop
         for i in range(abs(y1 - y0) + 1):
-            if (x0 > -1 and x0 < 501) and (smaller_y_value + i > -1 and smaller_y_value + i < 501):
+            if (x0 > -1 and x0 < 1024) and (smaller_y_value + i > -1 and smaller_y_value + i < 1024):
                 image.putpixel((x0, smaller_y_value + i), (255,255,255))
 
     # Else, find the equation of the line using two points and draw a line accordingly
@@ -33,7 +33,7 @@ def draw_basic_line(x0, y0, x1, y1):
                 x = smaller_x_value + i
                 y = (slope * x) + y_intercept
                 y = math.trunc(y)
-                if (x > -1  and x < 501) and (y > -1 and y < 501):
+                if (x > -1  and x < 1024) and (y > -1 and y < 1024):
                     image.putpixel((x,y), (255,255,255))
         # If Δx < Δy, or |x1-x0| < |y1-y0|, draw vertically |y1-y0| times.
         elif (abs(x1-x0)) < (abs(y1-y0)):
@@ -43,18 +43,18 @@ def draw_basic_line(x0, y0, x1, y1):
                y = smaller_y_value + i
                x = (y - y_intercept)/slope
                x = math.trunc(x)
-               if (x > -1  and x < 501) and (y > -1 and y < 501):
+               if (x > -1  and x < 1024) and (y > -1 and y < 1024):
                 image.putpixel((x,y), (255,255,255))
 
 coordinates = {
-    0: [1,1,1,1],
-    1: [2,1,1,1],
-    2: [2,2,1,1],
-    3: [1,2,1,1],
-    4: [1,1,2,1],
-    5: [2,1,2,1],
-    6: [2,2,2,1],
-    7: [1,2,2,1]
+    0: [-1,1,-1,1],
+    1: [1,1,-1,1],
+    2: [1,-1,-1,1],
+    3: [-1,-1,-1,1],
+    4: [-1,1,1,1],
+    5: [1,1,1,1],
+    6: [1,-1,1,1],
+    7: [-1,-1,1,1]
 }
 vertex_table = {
     0: [0,0],
@@ -67,14 +67,16 @@ vertex_table = {
     7: [0,0],
 }
 
+eye_transformation = trans.eyeCS(6,8,7.5,60,15)
+
 for i in coordinates:
-    matrix = np.dot(coordinates[i], trans.translate(0,0,0))
+    matrix = np.dot(coordinates[i], eye_transformation)
     coordinates[i] = matrix
     #print(f"{matrix[0]} {matrix[1]} {matrix[2]} {matrix[3]}")
 
 for i in coordinates:
-    x = ((7.5 * coordinates[i][0]) / (15 * coordinates[i][2])) * 250 + 250
-    y = ((7.5 * coordinates[i][1]) / (15 * coordinates[i][2])) * 250 + 250
+    x = (coordinates[i][0] / coordinates[i][2]) * 511.5 + 511.5
+    y = (coordinates[i][1] / coordinates[i][2]) * 511.5 + 511.5
     vertex_table[i] = [math.trunc(x), math.trunc(y)]
 
 
@@ -131,5 +133,6 @@ draw_basic_line(
     vertex_table[3][0], vertex_table[3][1],
     vertex_table[7][0], vertex_table[7][1]
 )
+
 
 image.show()
--- transformations.py
+++ transformations.py
@@ -1,5 +1,41 @@
+from functools import reduce
+from math import sqrt
+import numpy as np
+
+np.set_printoptions(formatter={'float' : lambda x: "{0:0.3f}".format(x)})
 def translate(Tx, Ty, Tz):
     return [[1,0,0,0],
             [0,1,0,0],
             [0,0,1,0],
             [Tx,Ty,Tz,1]]
+
+def eyeCS(x,y,z, D, S):
+    static_matrix =  [[1,0,0,0],
+                     [0,0,-1,0],
+                     [0,1,0,0],
+                     [0,0,0,1]]
+
+    y_rotation = [[-(y/(sqrt(x**2 + y**2))), 0, x/(sqrt(x**2 + y**2)), 0],
+                  [0,1,0,0],
+                  [-(x/(sqrt(x**2 + y**2))), 0 , -(y/(sqrt(x**2 + y**2))), 0],
+                  [0,0,0,1]]
+
+    x_rotation = [[1,0,0,0],
+                  [0,(sqrt(x**2 + y**2))/sqrt(z**2 + (sqrt(x**2 + y**2))**2), z/sqrt(z**2 + (sqrt(x**2 + y**2))**2), 0],
+                  [0, -(z/sqrt(z**2 + (sqrt(x**2 + y**2))**2)), (sqrt(x**2 + y**2))/sqrt(z**2 + (sqrt(x**2 + y**2))**2), 0],
+                  [0,0,0,1]]
+
+    z_rotation = [[1,0,0,0],
+                  [0,1,0,0],
+                  [0,0,-1,0],
+                  [0,0,0,1]]
+
+    view_distance = [[D/S,0,0,0],
+                     [0,D/S,0,0],
+                     [0,0,1,0],
+                     [0,0,0,1]]
+    product = reduce(np.dot, [translate(x,y,z), static_matrix, y_rotation, x_rotation, z_rotation, view_distance])
+
+    return product
+
+#eyeCS(6,8,7.5,60, 15)
